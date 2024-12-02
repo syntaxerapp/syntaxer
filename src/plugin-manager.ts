795da99cd9ac23dc411e10556b4195220395a1b2
awer <andrewkolegaev@gmail.com>
2024-12-02 21:31:08 +0300

    plugin system beta

--- src/plugin-manager.ts
+++ src/plugin-manager.ts
@@ -1,9 +1,24 @@
-import * as ShelfDependency from 'shelf-dependency'
-import { SamplePlugin } from './plugins/sample-plugin'
+// import * as ShelfDependency from 'shelf-dependency'
+// import { SamplePlugin } from './plugins/sample-plugin'
 
-const container = new ShelfDependency.Container()
+// const container = new ShelfDependency.Container()
 
-container.register('sample', SamplePlugin)
-container.register('logger', console)
+// container.register('sample', SamplePlugin)
+// container.register('logger', console)
 
-export { container }
+// export { container }
+
+import {PluginManager} from 'live-plugin-manager'
+
+const manager = new PluginManager()
+
+async function run() {
+  await manager.install('sample-plugin')
+
+  const moment = manager.require('moment')
+  console.log(moment().format())
+
+  await manager.uninstall('moment')
+}
+
+run()  
